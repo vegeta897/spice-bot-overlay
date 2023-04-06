ff67bf256514f6af60950ee29723f52ce6afce14
Devin Spikowski <vegeta897@gmail.com>
2023-04-06 17:11:49 -0400

    🚸 Slow down the train a bit

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 export const TRAIN = {
 	engineWidth: 98, // Padding included
 	carWidth: 83,
-	speed: 100, // Pixels per second
+	speed: 80, // Pixels per second
 	minLength: 5,
 	departDelay: 3 * 1000, // Milliseconds before train departs
 	endInfoDuration: 5 * 1000, // Milliseconds until info panel disappears
