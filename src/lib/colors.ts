e64b2aaf4401a155ba0a54abbb5cbab7530c9692
Devin Spikowski <vegeta897@gmail.com>
2023-05-05 04:37:18 -0400

    💄 Make gem colors pop a bit more

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -16,11 +16,14 @@ export function getGemColors(color: string) {
 	const cached = gemColorCache.get(color)
 	if (cached) return cached
 	const colorObject = Color(color)
-	const baseColor = colorObject.lightness(clamp(colorObject.lightness(), 40, 85)).hex()
-	const brighterColor = colorObject.lightness(75).rotate(10).hex()
-	const brightestColor = colorObject.lightness(92).rotate(20).hex()
-	const darkerColor = colorObject.lightness(55).rotate(-10).hex()
-	const darkestColor = colorObject.lightness(30).rotate(-20).hex()
+	const baseColor = colorObject
+		.lightness(clamp(colorObject.lightness(), 40, 85))
+		.saturate(0.05)
+		.hex()
+	const brighterColor = colorObject.lightness(75).rotate(15).hex()
+	const brightestColor = colorObject.lightness(92).rotate(25).hex()
+	const darkerColor = colorObject.lightness(55).rotate(-15).hex()
+	const darkestColor = colorObject.lightness(30).rotate(-25).hex()
 	const gemColors = {
 		baseColor,
 		brighterColor,
