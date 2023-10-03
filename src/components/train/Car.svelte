8bab4f4eab6d43758bb18fcda75191f261d5dccb
Devin Spikowski <vegeta897@gmail.com>
2023-10-03 03:34:57 -0400

    ♻️ Use Car component from lib

--- src/components/train/Car.svelte
+++ src/components/train/Car.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
 	import { carHop } from '../../lib/animations'
-	import { Body, ContainerSvg, Decal, Topper, Wheels } from 'grace-train-lib/components'
-	import { COLORS } from 'grace-train-lib'
 	import type { GraceTrainCar } from 'grace-train-lib/trains'
+	import { Car } from 'grace-train-lib/components'
 
 	export let car: GraceTrainCar
 	export let reverse: boolean
@@ -15,45 +14,4 @@
 </script>
 
 <!-- TODO: Increase size when depot site launches -->
-<ContainerSvg width="75" viewBox="0 0 375 300" bind:svgElement>
-	{#if typeof car === 'string'}
-		<Body name="boxy" stripeColor={car}></Body>
-	{:else}
-		<Body name={car.body} baseColor={car.bodyColor} popColor={car.bodyPopColor}>
-			<svelte:fragment slot="decals">
-				{#each car.decals as decal, d}
-					<Decal
-						name={decal.name}
-						fill={decal.fill}
-						transform={{
-							x: decal.x,
-							y: decal.y,
-							scale: decal.scale,
-							rotate: decal.rotate,
-						}}
-						params={decal.params}
-					/>
-				{/each}
-			</svelte:fragment>
-			<svelte:fragment slot="toppers" let:topLine>
-				{#each car.toppers as topper}
-					<Topper
-						{topLine}
-						name={topper.name}
-						colors={topper.colors}
-						position={topper.position}
-						offset={topper.offset}
-						scale={topper.scale}
-						rotate={topper.rotate}
-					/>
-				{/each}
-			</svelte:fragment>
-			<Wheels
-				rimColor={car.wheelColor}
-				capColor={car.bodyColor || COLORS.BASE[3]}
-				fromCenter={car.wheelFromCenter}
-				slot="wheels"
-			/>
-		</Body>
-	{/if}
-</ContainerSvg>
+<Car {car} width="75" bind:svgElement />
